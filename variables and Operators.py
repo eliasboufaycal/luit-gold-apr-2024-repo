fb496f8ef38005aaf5c8c13bfcc29f8d3d6c0ee4
eliasboufaycal <eliasboufaycal@hotmail.com>
2024-04-04 00:57:42 +0000

    variables and input

---
+++ variables and Operators.py
@@ -0,0 +1,20 @@
+# variables
+greeting = 'Hello, friend!'
+print(greeting)
+
+greeting = ("Hi, everybody!")
+print(greeting)
+
+income = 250_000
+lowtaxland_rate = 0.05
+ripoffland_rate = 0.43
+
+print('Your income is',income, 'and you would pay', income * lowtaxland_rate, 'income tax in Lowtaxland or', income * ripoffland_rate, 'income tax in Ripoffland. You Would save', income*ripoffland_rate - income * lowtaxland_rate , 'by paying taxes in Lowtaxland!')
+
+print('What is your name?')
+user_name = input()
+print('Hello,', user_name)
+
+Enter_your_login = input ('Enter your login: ')
+Enter_your_native_language = input ('Enter your native language: ')
+print('Your login is', Enter_your_login, 'and you speak', Enter_your_native_language)
